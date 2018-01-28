e52c55ec52b4295e35b5cc895e41fea40b7400f9
Joe Wemyss <support@javathenutt.com>
2018-01-28 15:29:17 +0000

    added guards to prevent unauthorised navigation

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-layout class="text-xs-center">
+      <v-flex><p class="headline">This is the Home page</p> </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  export default {
+    name: 'home'
+  };
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,49 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '@/store';
+import types from '@/store/types';
 import ViewAdminRequests from '@/components/ViewAdminRequests';
+import Home from '@/components/Home';
 
 Vue.use(Router);
-
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/admin/requests',
       name: 'admin-requests',
-      component: ViewAdminRequests
+      component: ViewAdminRequests,
+      meta: {
+        requiresAdmin: true
+      }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        requiresAuth: false
+      }
     }
   ]
 });
+router.beforeEach((to, from, next) => {
+  console.log('global authentication guard triggered');
+  console.log(to);
+  if (!to.meta || (!to.meta.requiresAdmin && !to.meta.requiresAuth)) {
+    console.log('route does not require auth');
+    return next();
+  }
+  if (to.meta.requiresAdmin) {
+    console.log('route requires admin privilages');
+    if (store.getters[types.authTypes.getters.isLoggedIn] && store.getters[types.authTypes.getters.isAdmin]) return next();
+    console.log('auth failed, returning to homepage');
+    return next('/');
+  }
+  if (to.meta.requiresAuth) {
+    console.log('route requires auth');
+    if (store.getters[types.authTypes.getters.isLoggedIn]) return next();
+  }
+  console.log('auth failed, returning to homepage');
+  return next('/');
+});
+export default router;
