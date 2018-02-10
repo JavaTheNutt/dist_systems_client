9ff4d15697bc3aa3ada3c8ff5000ff69312d26c6
Joe Wemyss <support@javathenutt.com>
2018-02-10 20:03:06 +0000

    Updated GenericAcceptRejectCard.vue
    Updated the generic component for yes/no dialogs. There were two main point s to this refactor, ui-updates and new event system.
    
    UI Updates:
    	The ui has been updated to take advantage of vuetify flex layouts. This will make the component more responsive.
    
    Event System:
    	This component used to take a function as a prop to respond to events within the component. This has been refactored so that the component will emit an event to its parent, in this case the generic dialog, to allow it to deal with the result.

--- src/components/GenericAcceptRejectCard.vue
+++ src/components/GenericAcceptRejectCard.vue
@@ -1,23 +1,36 @@
 <template>
   <v-card>
-    <v-card-title primary-title v-if="cardTitle.length > 0">
-      <h3 class="headline mb-0">{{cardTitle}}</h3>
-    </v-card-title>
-    <v-card-text subtitle v-if="cardText.length > 0">
-      <p class="subheading">{{cardText}}</p>
-    </v-card-text>
-    <v-card-actions>
-      <submit-form-button-group
-      :has-close="true"
-      :form-submittable="true"
-      :form-has-values="true"
-      positive-text="yes"
-      negative-text="no"
-      :loading="false"
-
-      />
-    </v-card-actions>
+    <v-container fluid>
+      <v-layout class="text-xs-center" column>
+        <v-flex class="text-xs-center">
+          <v-card-title v-if="cardTitle.length > 0">
+            <h3 class="text-xs-center headline">{{cardTitle}}</h3>
+          </v-card-title>
+        </v-flex>
+        <v-flex>
+          <v-card-text subtitle v-if="cardText.length > 0">
+            <p class="subheading">{{cardText}}</p>
+          </v-card-text>
+        </v-flex>
+        <v-flex>
+          <v-card-actions>
+            <submit-form-button-group
+              :has-close="false"
+              :form-submittable="true"
+              :form-has-values="true"
+              positive-text="yes"
+              negative-text="no"
+              @submit-clicked="yesClicked"
+              @reset-clicked="noClicked"
+              :loading="false"
+              align="center"
+            />
+          </v-card-actions>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-card>
+
 </template>
 <script>
   import SubmitFormButtonGroup from './SubmitFormButtonGroup';
@@ -33,14 +46,17 @@
     methods: {
       closeClicked () {
         console.log('close event caught in accept reject card');
+        this.$emit('reject');
         this.$emit('dialog-closed');
       },
       yesClicked () {
         console.log('yes clicked in accept reject card');
-        this.accept();
+        this.$emit('accept');
+        this.$emit('dialog-closed');
       },
       noClicked () {
         console.log('no clicked in accept reject card');
+        this.$emit('reject');
         this.$emit('dialog-closed');
       }
     }
