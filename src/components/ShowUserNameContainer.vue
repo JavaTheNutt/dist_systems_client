4ab1395877c8a679a3531380a1baf71295083b55
Joe Wemyss <support@javathenutt.com>
2018-02-10 20:11:53 +0000

    Created show username compoenent
    Created a basic show username component which just displays the users name.

---
+++ src/components/ShowUserNameContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-layout class="text-xs-left">
+      <v-flex class="headline"><h3>Welcome, {{username}}</h3></v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  import { mapGetters } from 'vuex';
+  import types from '@/store/types';
+
+  export default {
+    name: 'show-user-name-container',
+    computed: {
+      ...mapGetters({ username: types.authTypes.getters.getName })
+    }
+  };
+</script>
