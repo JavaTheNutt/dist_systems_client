7f44625096693b854a1e56e70b159d771a6a3bd1
Joe Wemyss <support@javathenutt.com>
2018-02-10 20:13:18 +0000

    Updated App.vue
    Updated the root component to attach the display username component above the primary router outlet.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app light>
     <navigation/>
     <v-content>
+      <show-user-name-container v-if="loggedIn"/>
       <router-view/>
     </v-content>
     <generic-dialog/>
@@ -11,7 +12,17 @@
 <script>
   import Navigation from './components/Navigation';
   import GenericDialog from './components/GenericDialog';
+  import { mapGetters } from 'vuex';
+  import types from './store/types';
+  import ShowUserNameContainer from './components/ShowUserNameContainer';
   export default {
-    components: { Navigation, GenericDialog }
+    components: {
+      ShowUserNameContainer,
+      Navigation,
+      GenericDialog
+    },
+    computed: {
+      ...mapGetters({ loggedIn: types.authTypes.getters.isLoggedIn })
+    }
   };
 </script>
